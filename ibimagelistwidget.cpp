4428537b8ad0f125c3f7c86fd1ba272179535b0c
Pietsch, Martin <martin.pietsch@tu-dresden.de>
2022-11-22 08:54:51 +0100

    add source code for the view widget

---
+++ ibimagelistwidget.cpp
@@ -0,0 +1,111 @@
+/* Copyright (C) 2022 Martin Pietsch <@pmfoss>
+   SPDX-License-Identifier: BSD-3-Clause */
+
+#include "ibimagelistwidget.hpp"
+
+/* Constructs the image list view with its item delegate and list model. */
+IBImageListWidget::IBImageListWidget(QWidget *parent)
+   : QListView(parent)
+{
+   QSize thumbsize(232,130);
+   QString path = QDir::currentPath();
+
+   this->setResizeMode(QListView::Adjust);
+   this->setViewMode(QListView::IconMode);
+
+   this->idDelegate = new IBItemDelegate(this);
+   this->setItemDelegate(this->idDelegate);
+
+   this->ifmImageModel = new IBImageListModel(path, thumbsize);
+   this->ifmImageModel->setSectionType(IBImageListModel::NoSection);
+   this->connect(this->ifmImageModel, SIGNAL(itemChanged(const QModelIndex &)), SLOT(update(const QModelIndex &)));
+
+   this->setModel(ifmImageModel);
+}
+
+/* Changes the visible size of the item delegate for the section items. */ 
+void IBImageListWidget::resizeEvent(QResizeEvent *event)
+{
+   this->idDelegate->resizeSectionSize(event->size());
+
+   QListView::resizeEvent(event);
+}
+
+/* Sets a path (path) for the list model. */
+void IBImageListWidget::setImagePath(const QString &path)
+{
+   this->ifmImageModel->setImagePath(path);
+}
+
+/* Returns the image path of the list model. */
+QString IBImageListWidget::getImagePath() const
+{
+   return this->ifmImageModel->getImagePath();
+}
+
+/* Sets a section type (type) for the list model and reselect the currently selected item. */ 
+void IBImageListWidget::setSectionType(IBImageListModel::IBListSectionType type)
+{
+   QModelIndex selidx = this->selectionModel()->currentIndex();
+   
+   selidx = this->ifmImageModel->setSectionType(type, selidx);
+   this->setCurrentIndex(selidx); 
+}
+
+/* Returns the section type of the list model. */
+IBImageListModel::IBListSectionType IBImageListWidget::getSectionType()
+{
+   return this->ifmImageModel->getSectionType();
+}
+
+/* Sets a order of image sorting for the list model and reselect the currently selected item. */
+void IBImageListWidget::setImageSortOrder(Qt::SortOrder order)
+{
+   QModelIndex selidx = this->selectionModel()->currentIndex();
+
+   selidx = this->ifmImageModel->setImageSortOrder(order, selidx);
+   this->setCurrentIndex(selidx); 
+}
+
+/* Returns the order of image sorting. */
+Qt::SortOrder IBImageListWidget::getImageSortOrder() const
+{
+   return this->ifmImageModel->getImageSortOrder();
+}
+
+/* Sets a order of section sorting for the list model and reselect the currently selected item. */
+void IBImageListWidget::setSectionSortOrder(Qt::SortOrder order)
+{
+   QModelIndex selidx = this->selectionModel()->currentIndex();
+
+   selidx = this->ifmImageModel->setSectionSortOrder(order, selidx);
+   this->setCurrentIndex(selidx); 
+}
+
+/* Returns the order of section sorting. */
+Qt::SortOrder IBImageListWidget::getSectionSortOrder() const
+{
+   return this->ifmImageModel->getSectionSortOrder();
+}
+
+/* Sets a field of image sorting for the list model and reselect the currently selected item. */
+void IBImageListWidget::setImageSortField(IBImageListModel::IBImageSortField field)
+{
+   QModelIndex selidx = this->selectionModel()->currentIndex();
+
+   selidx = this->ifmImageModel->setImageSortField(field, selidx);
+   this->setCurrentIndex(selidx); 
+}
+
+/* Returns the field of image sorting. */
+IBImageListModel::IBImageSortField IBImageListWidget::getImageSortField() const
+{
+   return this->ifmImageModel->getImageSortField();
+}
+
+/* Emits the signal selectionChanged with the first selected item. */
+void IBImageListWidget::selectionChanged(const QItemSelection &selected, const QItemSelection &deselected)
+{
+   QListView::selectionChanged(selected, deselected);
+   emit this->selectionChanged(selected.indexes().value(0));
+}
---
+++ ibimagelistwidget.hpp
@@ -0,0 +1,54 @@
+/* Copyright (C) 2022 Martin Pietsch <@pmfoss>
+   SPDX-License-Identifier: BSD-3-Clause */
+
+#ifndef H_IBIMAGELISTWIDGET
+#define H_IBIMAGELISTWIDGET
+
+#include <QItemSelection>
+#include <QListView>
+#include <QRect>
+#include <QRegion>
+#include <QResizeEvent>
+#include <QScrollBar>
+#include <QWidget>
+
+#include "ibimagelistmodel.hpp"
+#include "ibitemdelegate.hpp"
+
+class IBImageListWidget : public QListView
+{
+   Q_OBJECT 
+
+   public:
+      IBImageListWidget(QWidget *parent = nullptr);
+      void setSectionType(IBImageListModel::IBListSectionType type);
+      IBImageListModel::IBListSectionType getSectionType();
+
+      void setSectionSortOrder(Qt::SortOrder order);
+      Qt::SortOrder getSectionSortOrder() const;
+
+      void setImageSortOrder(Qt::SortOrder order);
+      Qt::SortOrder getImageSortOrder() const;
+      void setImageSortField(IBImageListModel::IBImageSortField field);
+      IBImageListModel::IBImageSortField getImageSortField() const;
+
+      QString getImagePath() const;
+
+   signals:
+      void selectionChanged(const QModelIndex &index);
+
+   public slots:
+      void setImagePath(const QString &path);
+
+   protected:
+      void resizeEvent(QResizeEvent *event) override;
+      void selectionChanged(const QItemSelection &selected, const QItemSelection &deselected) override;
+
+   private:
+      /* contains the ItemDelegate object of the view */ 
+      IBItemDelegate *idDelegate;
+      /* contains the List Model of the view */
+      IBImageListModel *ifmImageModel;
+};
+
+#endif /*H_IBIMAGELISTWIDGET*/
